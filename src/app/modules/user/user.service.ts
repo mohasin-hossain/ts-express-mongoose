94bae9897f8718aa29b1b97bc34f761d87dd41ad
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-02-25 14:23:28 +0100

    Field filtering to exclusively retrieve the necessary information

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -10,11 +10,28 @@ const createUserIntoDB = async (userData: TUser) => {
 };
 
 const getAllUsersFromDB = async () => {
-    const result = await User.find();
-    return result;
-}
+    const result = await User.find(
+      {},
+      {
+        username: 1,
+        fullName: 1,
+        age: 1,
+        email: 1,
+        address: 1,
+        _id: 0,
+      },
+    );
+
+  // OR
+
+//   const result = await User.find().select(
+//     'username fullName age email address -_id',
+//   );
+
+  return result;
+};
 
 export const UserServices = {
   createUserIntoDB,
-  getAllUsersFromDB
+  getAllUsersFromDB,
 };
